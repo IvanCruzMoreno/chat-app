b4f4c5cef216e7d7d1d1b26d40c28b4befa62d14
ivan <ivanmoreno405@gmail.com>
2021-01-04 16:48:47 -0600

    i did what was necessary to send and receive the message

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Client} from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+import {Mensaje} from './models/mensaje';
 
 @Component({
   selector: 'app-chat',
@@ -10,6 +11,9 @@ import * as SockJS from 'sockjs-client';
 export class ChatComponent implements OnInit {
 
   private client: Client;
+  conectado: boolean = false;
+  mensaje: Mensaje = new Mensaje();
+  mensajes: Mensaje[] = [];
 
   constructor() { }
 
@@ -20,8 +24,28 @@ export class ChatComponent implements OnInit {
     }
     this.client.onConnect = (frame) => {
       console.log('Conectados: '+ this.client.connected + ' : ' + frame);
+      this.conectado = true;
+
+      this.client.subscribe('/chat/mensaje', e => {
+        let mensaje: Mensaje = JSON.parse(e.body) as Mensaje;
+        mensaje.fecha = new Date(mensaje.fecha);
+        this.mensajes.push(mensaje);
+        console.log(mensaje);
+      })
     }
+    this.client.onDisconnect = (frame) => {
+      console.log('Conectados: '+ !this.client.connected + ' : ' + frame);
+      this.conectado = false;
+    }
+  }
+  conectar(){
     this.client.activate();
   }
-
+  desconectar(){
+    this.client.deactivate();
+  }
+  enviarMensaje(): void {
+    this.client.publish({destination: '/app/mensaje', body: JSON.stringify(this.mensaje)});
+    this.mensaje.texto='';
+  }
 }
